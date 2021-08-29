897ed779e589d011812eff11ba7aacaeb1d824a2
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-28 23:20:52 -0300

    starting build project

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const path = require('path');
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
     alwaysOnTop: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
