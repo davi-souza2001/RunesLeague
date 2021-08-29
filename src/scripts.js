4babea2e451c66a7ef60fb1296de23324525561d
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-28 22:05:24 -0300

    teste but app work

--- src/scripts.js
+++ src/scripts.js
@@ -5,5 +5,6 @@ function getchamp(event) {
     event.preventDefault();
     console.log(`Peguei o champ e ele é o ${champion.value}`);
     window.location.href = `https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`;
+    /* const result = window.open(`https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`); */
 }
 
