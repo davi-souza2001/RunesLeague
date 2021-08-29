2160226d5701caeeb68c72f1ef03aa9e8cc5ae7a
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-28 21:44:33 -0300

    Function app working

---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+let champion = document.querySelector(".formcontenttext");
+
+
+function getchamp(event) {
+    event.preventDefault();
+    console.log(`Peguei o champ e ele é o ${champion.value}`);
+    window.location.href = `https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`;
+}
+
