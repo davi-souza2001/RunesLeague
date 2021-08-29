0458697e4620d102c5fc5a01a3fbf4c064351fea
davi-souza2001 <davisouza2001dv@gmail.com>
2021-08-28 23:14:18 -0300

    APP COMPLETE :tada:

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,6 @@ let champion = document.querySelector(".formcontenttext");
 function getchamp(event) {
     event.preventDefault();
     console.log(`Peguei o champ e ele é o ${champion.value}`);
-    window.location.href = `https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`;
-    /* const result = window.open(`https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`); */
+    /* window.location.href = `https://www.leagueofgraphs.com/pt/champions/runes/${champion.value}`; */
+    window.location.href = `https://www.runas.lol/${champion.value}/`;
 }
